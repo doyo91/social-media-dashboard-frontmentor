1e1c84546aa93083ec169db3619a3fc9a67ff09b
doyo91 <doyo_91@hotmail.com>
2020-07-20 17:52:05 +0200

    Dark mode

--- js/app.js
+++ js/app.js
@@ -1,9 +1,32 @@
 const checkbox = document.getElementById("checkbox");
+const currentTheme = localStorage.getItem('theme');
 
-if (window.matchMedia("(prefers-color-scheme)").matches) {
-  checkbox.setAttribute("checked", true);
+// if (window.matchMedia("(prefers-color-scheme)").matches) {
+//   checkbox.setAttribute("checked", true);
+// }
+
+// checkbox.addEventListener("change", function (event) {
+//   document.body.classList.toggle("is-dark-mode");
+// });
+
+
+
+if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+  
+    if (currentTheme === 'dark') {
+        toggleSwitch.checked = true;
+    }
 }
 
-checkbox.addEventListener("change", function (event) {
-  document.body.classList.toggle("is-dark-mode");
-});
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
+    }
+    else {        document.documentElement.setAttribute('data-theme', 'light');
+          localStorage.setItem('theme', 'light');
+    }    
+}
+
+checkbox.addEventListener('change', switchTheme, false);
